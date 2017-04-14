d936c8d59b140716b1a44650047a1f6ddbb69892
hammad-naqi <shummad.naqi@gmail.com>
2017-04-14 08:48:08 -0700

    Add files via upload

---
+++ src/models/items.ts
@@ -0,0 +1,15 @@
+export interface Items {
+  itemId: number;
+  itemName: string;
+  desc: string;
+  price: string;
+  cuisinesName: string;
+  categoryName: string;
+  categoryDesc: string;
+  picture: string;
+  lat: string;
+  long: string;
+  supplierName: string;
+  supplierId: number;
+  mediaId: number;
+}
